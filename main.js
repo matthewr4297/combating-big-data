cae7be4b4676904643edbd09e6efd3d6cbcbb761
Matthew Roy <matthewr4297@hstat.org>
2019-05-07 13:51:25 +0000

    carousel done - js notifier added - more js required

--- main.js
+++ main.js
@@ -1,3 +1,11 @@
 $(function(){
-          
-      });
+        $('#img-1-1').dblclick(function(){
+            console.log("JS is working properly")
+        })
+    $('#img-1-2').dblclick(function(){
+            console.log("JS is working properly")
+        })
+        $('#img-1-3').dblclick(function(){
+            console.log("JS is working properly")
+        })
+});
